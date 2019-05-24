50767f3e69d81ca9906b10068f332c2eb5148a55
Diana <d_quinlan061006@yahoo.com>
2019-05-23 21:01:27 -0400

    finished survey questions, added friends to the array.

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,14 +3,26 @@
 var surveyArray = [
     {
         name:"Diana",
-        photo:"photourl",
-       scores: ["1","1","1","1","1","1","1","1","1","1"]
+        photo:"https://img1.wsimg.com/isteam/ip/68b8ed37-e4a0-4aef-916d-2876d63ae59d/55c8ca5b-76af-486c-b037-642845de0255.jpg/:/cr=t:11.54%25,l:11.54%25,w:76.92%25,h:76.92%25/rs=w:555,m,cg:true",
+       scores: ["3","3","3","2","3","5","3","3","3","2"]
        
     },
     {
-        name:"Mike",
-        photo:"photourl2",
-       scores: ["2","1","4","1","5","1","4","1","1","1"]
+        name:"Bruce",
+        photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3zyp4XqhDqg4kKK1pVI7CHyYXTX-L03ymM-3JlOpjtoWsRIqa",
+       scores: ["1","2","1","3","4","3","1","1","3","5"]
+       
+    },
+    {
+        name:"Clark",
+        photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZljWlSwSMeo-3dn4Qrrcv94lvTYb6Znfi4TB8c4WQaEvgYHQo",
+       scores: ["5","5","1","4","2","5","4","1","1","3"]
+       
+    },
+    {
+        name:"Natasha",
+        photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDv2NuM4aRwx7NWp6vjgm3OQy7qrURcSNbNH_9ymc0sJVlVRsj",
+       scores: ["3","1","1","1","5","4","3","1","5","5"]
        
     }
 ];
